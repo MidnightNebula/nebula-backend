e3d7d5888ac6e7cf3a1cb5de9043543260b25a78
NickoLu <hick64077@gmail.com>
2025-12-19 04:38:33 +0500

    files: delete docker file config: delete watch from package scripts refactor: add spaces in code

--- src/app.ts
+++ src/app.ts
@@ -12,9 +12,11 @@ app.use("*", authMiddleware);
 app.use("/api/auth/*", authCors);
 
 const routes = [authRouter] as const;
+
 app.get("/", (c) => {
   return c.text("Hello world");
 });
+
 routes.forEach((route) => {
   app.basePath("/api").route("/", route);
 });
